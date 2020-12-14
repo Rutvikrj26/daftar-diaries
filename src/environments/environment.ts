45ac643f71ce90392b6e903cac358aa9c7a2e177
root <root@Rutvik.localdomain>
2020-12-14 23:35:11 +0530

    Initial Buid add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,14 @@ export const environment = {
   production: false,
   
   firebaseConfig :{
- apiKey: "",
- authDomain: "",
- databaseURL: "",
- projectId: "",
- storageBucket: "",
- messagingSenderId: "",
- appId: "",
- measurementId: ""
+    apiKey: "AIzaSyAfuiRHvucnodn6W-xa7ttRSanFsrq0YKI",
+    authDomain: "vuegram-2b5a0.firebaseapp.com",
+    databaseURL: "https://vuegram-2b5a0-default-rtdb.firebaseio.com",
+    projectId: "vuegram-2b5a0",
+    storageBucket: "vuegram-2b5a0.appspot.com",
+    messagingSenderId: "344946394525",
+    appId: "1:344946394525:web:54505d353090a15a781b74",
+    measurementId: "G-CHJ72HC7X6"
 
 }
 }
